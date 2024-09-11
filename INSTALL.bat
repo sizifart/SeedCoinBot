95d134a5013bf97cd23d82ecbeca0154f12257e2
Foad Davoodi <myf.davoodi@gmail.com>
2024-09-11 23:01:35 +0330

    Add files via upload

---
+++ INSTALL.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Creating virtual environment...
+python -m venv venv
+echo Activating virtual environment...
+call venv\Scripts\activate
+echo installing wheel for faster installing
+pip install wheel
+echo Installing dependencies...
+pip install -r requirements.txt
+echo Copying .env-example to .env...
+copy .env-example .env
+echo Please edit the .env file to add your API_ID and API_HASH.
+pause
