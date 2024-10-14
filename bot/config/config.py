212f985cc1090f5a7ffb777723b860ee87705ea3
Foad Davoodi <myf.davoodi@gmail.com>
2024-10-14 22:23:45 +0330

    Add files via upload

---
+++ bot/config/config.py
@@ -0,0 +1,46 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+
+    API_ID: int
+    API_HASH: str
+
+    REF_LINK: str = "t.me/seed_coin_bot/app?startapp=558455838"
+
+    AUTO_UPGRADE_STORAGE: bool = True
+    AUTO_UPGRADE_MINING: bool = True
+    AUTO_UPGRADE_HOLY: bool = True
+    AUTO_CLEAR_TASKS: bool = True
+    AUTO_START_HUNT: bool = True
+
+    AUTO_SELL_WORMS: bool = False
+    QUANTITY_TO_KEEP: dict = {
+        "common": {
+            "quantity_to_keep": 2,
+            "sale_price": 1
+        },
+        "uncommon": {
+            "quantity_to_keep": 2,
+            "sale_price": 0
+        },
+        "rare": {
+            "quantity_to_keep": -1,
+            "sale_price": 0
+        },
+        "epic": {
+            "quantity_to_keep": -1,
+            "sale_price": 0
+        },
+        "legendary": {
+            "quantity_to_keep": -1,
+            "sale_price": 0
+        }
+    }
+
+    USE_PROXY_FROM_FILE: bool = False
+
+settings = Settings()
+
+
---
+++ bot/core/headers.py
@@ -0,0 +1,15 @@
+headers = {
+    'accept': '*/*',
+    'accept-language': 'en-US;q=0.9,en;q=0.8,id;q=0.7',
+    'origin': 'https://cf.seeddao.org',
+    'priority': 'u=1, i',
+    'referer': 'https://cf.seeddao.org/',
+    'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'telegram-data': 'tokens',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+}
